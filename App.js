4476ad6d69e34fcb341cf62bb43d687af55993a2
Jyotiraditya Solanki <jyotiraditya.solanki@impetus.com>
2023-09-20 17:15:23 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,18 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello from React"
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(parent);
